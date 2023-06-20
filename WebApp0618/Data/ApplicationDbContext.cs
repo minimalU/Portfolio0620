29a21ad666fceea10006af5588999fc946dfcf04
YJ <yujungpark@outlook.com>
2023-06-20 01:39:31 -0400

    Add README.md.

---
+++ WebApp0618/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp0618.Models;
+
+namespace WebApp0618.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApp0618.Models.Post>? Post { get; set; }
+    }
+}
---
+++ WebApp0618/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp0618.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        // constructor
+        public Post()
+        {
+
+        }
+    }
+}
